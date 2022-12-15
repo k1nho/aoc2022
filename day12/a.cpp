d87d3515cbccf9ea0fa903f97e0fd536d017385c
k1nho <kinhong99@gmail.com>
2022-12-14 19:37:42 -0500

    p1: bfs

---
+++ day12/a.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <ios>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+typedef pair<int, pair<char, pair<int, int>>> pii;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<vector<char>> m;
+    queue<pii> q;
+    int row = 0;
+
+    string s;
+    while(cin >> s) {
+        vector<char> temp;
+        for(auto ch : s) {
+            if(ch == 'S') {
+                temp.push_back('a');
+                q.push({0, {'a', {row, 0}}});
+                continue;
+
+            }
+            temp.push_back(ch);
+        }
+        row++;
+        m.push_back(temp);
+    }
+    
+    int M = m.size(), N = m[0].size();
+    vector<pair<int, int>> dirs{{1,0}, {-1,0}, {0,-1}, {0,1}};
+    auto checkBounds = [&](int r, int c) -> bool {
+        if(r >= 0 && r < M && c >= 0 && c < N) return true;
+        return false;
+    };
+
+    auto checkTransition = [&](char from, char to) -> bool {
+        if(from - to >= 0) return true;
+        else if(from - to == -1) return true;
+        return false;
+    };
+
+    bool notFound = true;
+    int moves = INT_MAX;
+    while(!q.empty() &&  notFound) {
+        for(int i = 0; i < q.size(); i++ ){
+            auto [x,y] = q.front().second.second;
+            char nodeval = q.front().second.first;
+            int dist = q.front().first;
+            m[x][y] = '*';
+            q.pop();
+
+            for(auto p : dirs) {
+                int nr = x + p.first;
+                int nc = y + p.second;
+                if(checkBounds(nr, nc) && m[nr][nc] == 'E' && checkTransition(nodeval, 'z')) {
+                    moves = min(moves, dist+1);
+                    notFound = false;
+                }
+                else if(checkBounds(nr, nc) && m[nr][nc] == 'E') continue;
+                else if(checkBounds(nr, nc) && m[nr][nc] != '*' && checkTransition(nodeval, m[nr][nc])) {
+                    q.push({dist+1, {m[nr][nc] , {nr, nc}}});
+                    m[nr][nc] = '*';
+                }
+            }
+        }
+    }
+
+    cout << "total moves is " << moves << '\n'; 
+
+
+}
