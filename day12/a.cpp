df9c21f3278217164a0525a4809ebccc7d8f5e46
k1nho <kinhong99@gmail.com>
2022-12-14 21:36:18 -0500

    refactor

--- day12/a.cpp
+++ day12/a.cpp
@@ -38,6 +38,7 @@ int main() {
     
     int M = m.size(), N = m[0].size();
     vector<pair<int, int>> dirs{{1,0}, {-1,0}, {0,-1}, {0,1}};
+
     auto checkBounds = [&](int r, int c) -> bool {
         if(r >= 0 && r < M && c >= 0 && c < N) return true;
         return false;
@@ -50,32 +51,28 @@ int main() {
     };
 
     bool notFound = true;
-    int moves = INT_MAX;
-    while(!q.empty() &&  notFound) {
+    while(!q.empty() && notFound) {
         for(int i = 0; i < q.size(); i++ ){
             auto [x,y] = q.front().second.second;
             char nodeval = q.front().second.first;
             int dist = q.front().first;
             m[x][y] = '*';
+            if(nodeval == 'E') {cout << "total moves to get to target " << dist << '\n'; notFound = false; break;}
             q.pop();
 
             for(auto p : dirs) {
                 int nr = x + p.first;
                 int nc = y + p.second;
-                if(checkBounds(nr, nc) && m[nr][nc] == 'E' && checkTransition(nodeval, 'z')) {
-                    moves = min(moves, dist+1);
-                    notFound = false;
-                }
-                else if(checkBounds(nr, nc) && m[nr][nc] == 'E') continue;
-                else if(checkBounds(nr, nc) && m[nr][nc] != '*' && checkTransition(nodeval, m[nr][nc])) {
-                    q.push({dist+1, {m[nr][nc] , {nr, nc}}});
-                    m[nr][nc] = '*';
+                
+                if(checkBounds(nr , nc)) {
+                    char ch = m[nr][nc] == 'E' ? 'z' : m[nr][nc];
+                    if(checkTransition(nodeval, ch) && m[nr][nc] != '*'){
+                        q.push({dist +1, {m[nr][nc] , {nr, nc}}});
+                        m[nr][nc] = '*';
+                    }
                 }
             }
         }
     }
-
-    cout << "total moves is " << moves << '\n'; 
-
 
 }
